8e6a3ae1371c7ea373b4df2d913ff9356d0f01d8
Tinashe <Tinaguhwa@gmail.com>
2023-09-21 14:10:33 -0700

    a function that prints before a main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,9 +3,9 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * print_first - prints a sentence before the main
+ *first - it prints a sentence before the main
  */
-void print_first(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
